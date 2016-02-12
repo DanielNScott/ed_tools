2c1c922001a6a8d3bf0cee79e5ebe6b13f7e8ece
Daniel Scott <dnscott87@gmail.com>
2016-02-12 09:45:47 -0800

    Better varnames in test_pso_curv_covar.
    Also renamed curvature_covar_opt_test.m because it was originally intended
    to include test_pso_curv_covar.m, but just became a covar/eigenval toy.

--- test_pso_curv_covar.m
+++ test_pso_curv_covar.m
@@ -2,15 +2,13 @@ function [ ] = test_pso_curv_covar( )
 %TEST_PSO_CURV_COVAR Summary of this function goes here
 %   Detailed explanation goes here
 
-close all
-
-% Set PSO params and problem specs.
-nps  = 20;
-niter= 40;
-bnds = [-3,3; -3,3];
-inc  = 0.1;
-fn   = @(x) rosenbrock(x);
-gn   = @(x) rosenbrock(x');
+% Set PSO params and problem specifications
+nps   = 20;
+niter = 60;
+bnds  = [-3,3; -3,3];
+inc   = 0.1;
+fn    = @(x) rosenbrock(x);
+gn    = @(x) rosenbrock(x');
 
 % Find the minimum.
 [sol, trace] = particle_swarm(bnds,nps,niter,fn);
@@ -23,7 +21,7 @@ diffs     = sqrt(sum((flat_locs - big_sol).^2,2));
 
 % Mask out 10th percentile of data in terms of euclidean distance.
 % These particles will be used to construct the response surface.
-msk = diffs < prctile(diffs,2);
+msk   = diffs < prctile(diffs,2);
 blocs = flat_locs(msk,:);
 
 % Find the minimum and maximum locations to plot particles in
@@ -35,34 +33,34 @@ blocrng = blocmax - blocmin;
 blocinc = blocrng/50;
 
 % Get the response surface aka second order fit surface.
-[fh,gof] = fit([blocs(:,1),blocs(:,2)],flat_objs(msk),'poly22');
+[funHand,goodOfFit] = fit([blocs(:,1),blocs(:,2)],flat_objs(msk),'poly22');
 
 % Determine the x-domain and y-domain (the parameter space grid)
 % used in plotting the fit surface.
 xdom = (blocmin(1)-blocrng(1)):blocinc(1):(blocmin(1)+blocrng(1));
 ydom = (blocmin(2)-blocrng(2)):blocinc(2):(blocmin(2)+blocrng(2));
 
-[X,Y] = meshgrid(xdom,ydom);
-F = fh(X(:),Y(:));
-F = reshape(F,length(xdom),length(ydom));
+[meshVec1,meshVec2] = meshgrid(xdom,ydom);
+funVals = funHand(meshVec1(:),meshVec2(:));
+funVals = reshape(funVals,length(xdom),length(ydom));
 
 % Create best-point-transects of fit at the solution.
 % SX     = size(X);
 btranx = [repmat(sol(1,1),1,length(xdom)); ydom]';
 btrany = [xdom; repmat(sol(1,2),1,length(xdom))]';
 
-btranxval = fh(btranx);
-btranyval = fh(btrany);
+btranxval = funHand(btranx);
+btranyval = funHand(btrany);
 
 % Get the actual objectives on the mesh-grid so we can plot the
 % objective function along with the fit.
-obj = rosenbrock([X(:),Y(:)]);
+obj = rosenbrock([meshVec1(:),meshVec2(:)]);
 obj = reshape(obj,length(xdom),length(ydom));
 
 % Gradient from fit at optimization solution:
-[fx, fy, fxx, fxy, fyy] = differentiate(fh,sol(1),sol(2));
-gradNorm = sqrt(fx^2 + fy^2);
-unitDir  = -1*[fx/gradNorm, fy/gradNorm];
+[dfdx, dfdy, dfdxx, dfdxy, dfdyy] = differentiate(funHand,sol(1),sol(2));
+gradNorm = sqrt(dfdx^2 + dfdy^2);
+unitDir  = -1*[dfdx/gradNorm, dfdy/gradNorm];
 
 % Find & evaluate the minimum of the response surface;
 %resp_sol = fminsearch(fh,sol);
@@ -79,15 +77,19 @@ subaxis(2,2,2)
    hold on
    scatter(blocs(:,1),blocs(:,2))
    scatter(sol(1),sol(2),'or')            % Plot the solution
-   quiver(sol(1),sol(2),-fx,-fy)          % Plot the local downhill direction.
-   quiver(sol(1),sol(2), fy,-fx)          % Plot the local characteristic direction.
+   quiver( sol(1),sol(2),-dfdx,-dfdy)     % Plot the local downhill direction.
+   quiver( sol(1),sol(2), dfdy,-dfdx)     % Plot the local characteristic direction.
    hold off
+   
+   disp(['dfdxx: ', num2str(dfdxx)])
+   disp(['dfdyy: ', num2str(dfdyy)])
+   disp(['dfdxy: ', num2str(dfdxy)])
    
    % Local characteristic subspaces? Relationship to covariation?
 
 subaxis(2,2,3)
    % Plot the response surface
-   surf(xdom,ydom,F,'EdgeColor','none')
+   surf(xdom,ydom,funVals,'EdgeColor','none')
    alpha(0.5)
 
    set(gca,'XLim',[xdom(1),xdom(end)])
@@ -122,9 +124,7 @@ subaxis(2,2,3)
           ,'X-Transect Proj.','Y-Transect Proj.','Solution'})
 
 %gof.adjrsquare
-pause
-
-
+%pause
 
 end
 
