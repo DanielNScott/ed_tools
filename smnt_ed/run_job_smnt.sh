be9e81ab2cd937cd3ed303032eed08092f186c36
DanielNScott <dnscott87@gmail.com>
2016-03-02 16:11:36 -0500

    Jobs succeed, but smnt seems to hang. Probably not related to these scripts.

--- smnt_ed/run_job_smnt.sh
+++ smnt_ed/run_job_smnt.sh
@@ -4,4 +4,4 @@
 path_tools='/n/moorcroftfs2/dscott/ed_tools_dev/' #
 
 #----- Run Matlab -------------------------------------------------------------------------#
-matlab -nojvm -nodisplay -nosplash -r addpath\(genpath\(\'${path_tools}\'\)\),run_job_smnt\(${1}\),exit
+matlab -nojvm -nodisplay -nosplash -r addpath\(genpath\(\'${path_tools}\'\)\),run_job_smnt\(${1},${2}\),exit
