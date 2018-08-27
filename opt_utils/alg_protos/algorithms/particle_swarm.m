e636df5d18276fc9f040313cc9dd7653e0a156d5
Daniel Scott <dnscott87@gmail.com>
2018-08-27 16:54:27 -0400

    Particle swarm experimentation...

--- opt_utils/alg_protos/algorithms/particle_swarm.m
+++ opt_utils/alg_protos/algorithms/particle_swarm.m
@@ -2,25 +2,25 @@ function [sol, trace] = particle_swarm( bnds, niter, nps, fn )
 %PSO This is a first pass at a particle swarm optimizer.
 %   Detailed explanation goes here
 
-%----------------------------------------------------------------------------------------------%
-% Test Settings
-%----------------------------------------------------------------------------------------------%
-% Search space as grid.
-ss_cent = mean(bnds,2)';                                    % Specify center 
-ss_rng  = range(bnds');                                     % Specify ranges of x,y vals, as +/-
-ndim    = numel(ss_cent);
+% Useful constants:
+ndim    = length(bnds);
 
-%----------------------------------------------------------------------------------------------%
-% Von-Neumman Topology Canonical PSO.
-% Canonical PSO is has only 2 influences on a particle, and constriction coefficients.
-%----------------------------------------------------------------------------------------------%
-phi_1  = 4.1;                                               % Strength of pbest attractor, > 4.
-phi_2  = 4.1;                                               % Strength of gbest attractor, > 4.
-phi    = phi_1 + phi_2;                                     % Shorthand
-chi    = 2/(phi - 2 + sqrt(phi^2 - 4*phi));                 % Constrictor
-%Vmax   = (ss_rng - ss_cent)*2;                             % Limit velocity to dynamic range.
+% PSO parameter settings:
+update_nbrhd  = 1;
+adapt_inertia = 1;
+adapt_nnbr    = 1;
 
-% Imported back from 'production':
+min_neighbor_frac = 0.25;
+min_inert         = 0.1;
+max_inert         = 1.1;
+start_inert       = 0.37;
+
+cSelf = 1.49;
+cSoc  = 1.49;
+inert = repmat(start_inert, nps, 1);
+neighbor_frac = min_neighbor_frac;
+
+% Randomly initialize locations and vebest_loc_selfties
 spread = repmat(bnds(:,2) - bnds(:,1),1,nps);               % Compute parameter spreads
 cents  = repmat(bnds(:,1) + spread(:,1)/2 ,1,nps);          % Compute centers of ranges
          
@@ -34,25 +34,10 @@ vels = vels';
 %quiver(locs(:,1), locs(:,2), vels(:,1), vels(:,2), 'o')
 
 bests = fn(locs);                                           % Initialize particle objectives
-loci  = locs;                                               % Set locations of bests as current
+best_loc_self  = locs;                                               % Set locations of bests as current
+best_loc = best_loc_self(bests == min(bests),:);
 
-% Create neighborhoods
-prime_fact = factor(nps);
-vnt_dim1   = prod(prime_fact(1:end-1));
-vnt_dim2   = prime_fact(end);
-template   = zeros(1,nps);
-for ip = 1:nps;
-   b_ind = mod(ip+1,nps);
-   u_ind = mod(ip-1,nps);
-   r_ind = mod(ip+vnt_dim2,nps);
-   l_ind = mod(ip-vnt_dim2,nps);
-   
-   nbrs = [b_ind, u_ind, r_ind, l_ind];
-   nbrs(nbrs == 0) = nps;
-   nbrhd(ip,:) = nbrs;
-end
-
-locg = nan(nps,ndim);
+best_loc_glob = nan(nps,ndim);
 trace.loc  = nan(niter,nps,ndim);
 trace.vals = nan(niter,nps);
 
@@ -60,39 +45,137 @@ trace.states     = nan(niter,ndim);
 trace.objectives = nan(niter,1);
 
 % Loop through actual PSO steps
+counter = 0;
+%counter = zeros(nps,1);
+
 for iter = 1:niter
-   U_1  = rand(nps,ndim)*phi_1;
-   U_2  = rand(nps,ndim)*phi_2;
+  
+   randSelf = rand(nps, ndim);
+   randSoc  = rand(nps, ndim);
+
+   nbrhd = create_neighborhoods(nps, update_nbrhd, neighbor_frac);
    
+   % Get location of global best 
    for ip = 1:nps
       best = min(bests(nbrhd(ip,:)'));
       if sum(bests == best) > 1
          ind = find(bests == best,1);
-         locg(ip,:) = locs(ind,:);
+         best_loc_glob(ip,:) = best_loc_self(ind,:);
       else
-         locg(ip,:) = locs(bests == best,:);
+         best_loc_glob(ip,:) = best_loc_self(bests == best,:);
       end
    end
-      
-   vels = chi*(vels + U_1.*(loci - locs) + U_2.*(locg - locs));
+   
+   % Standard velocity update
+   vels = inert.*vels + cSelf*randSelf.*(best_loc_self - locs) + cSoc*randSoc.*(best_loc_glob - locs);
+   
+   % Add a decaying repulsion from other particles
+   %for ip = 1:nps
+   %    displacements = locs - locs(ip,:);
+   %    dists = sqrt(sum(displacements.^2, 2));
+   %    
+   %    msk = setdiff(1:nps,ip);
+   %    unit_vecs = displacements(msk,:) ./ repmat(dists(msk),1,2);
+   %    speed = sqrt(sum(vels(ip,:).^2,2));
+   %    force = -unit_vecs ./ repmat(dists(msk).^2,1,2);
+   %    accel = sum(force)/nps * 1/2 * speed * (niter - iter)/iter;
+   %    
+   %    accel(isnan(accel)) = 0;
+   %    vels(ip,:) = vels(ip,:) + accel;
+   %end
+   
    locs = locs + vels;
    
-   objs = fn(locs);
-   msk  = objs < bests;
-
-   loci(msk,:) = locs(msk,:);
-   bests(msk)  = objs(msk);
+   % Random reshuffling of the worst locs
+   %displacements = locs - best_loc;
+   %dists = sqrt(sum(displacements.^2, 2));
+   %shuffle_msk = dists < prctile(dists,5);
+   %locs(shuffle_msk,:) = ((rand(2,sum(shuffle_msk)) - 0.5)' .* spread(:,1)' + best_loc) *(niter - iter + 1)/niter;
    
-   best_loc = loci(find(bests == min(bests),1),:);
+   % Clip locations and velocities
+   range = bnds(:,2) - bnds(:,1);
+   for dim = 1:ndim
+      msk = locs(:,dim) < bnds(dim,1);
+      locs(msk,dim) = bnds(dim,1);
+      vels(msk,dim) = -vels(msk,dim);
+      
+      msk = locs(:,dim) > bnds(dim,2);
+      locs(msk,dim) = bnds(dim,2);
+      vels(msk,dim) = -vels(msk,dim);
+      
+      % Keep velocities from jumping between edges
+      msk = abs(vels(:,dim)) > range(dim);
+      vels(msk,dim) = vels(msk,dim)/1.5;
+   end
+   
+   objs = fn(locs);
+   improved = objs < bests;
+   improved_globally = any(objs < min(min(bests)));
+
+   best_loc_self(improved,:) = locs(improved,:);
+   bests(improved)  = objs(improved);
+
+   % -- Adaptation a-la MATLAB -- %
+   if any(improved_globally)
+      counter = max(0, counter - 1);
+      neighbor_frac = min_neighbor_frac;
+   else
+      counter = counter + 1;
+      neighbor_frac = neighbor_frac + min_neighbor_frac;
+      neighbor_frac = min(neighbor_frac*nps, nps - 1)/nps;
+   end
+   
+   if adapt_inertia == 1
+       if counter < 2
+          inert = max(min_inert, min(max_inert, 2*inert)); 
+        elseif counter > 5
+          inert = max(min_inert, min(max_inert, 0.5*inert));        
+       end
+   end
+   %-------------------------------%
+   
+   best_loc = best_loc_self(bests == min(bests),:);
    
    trace.states    (iter,:)   = locs(find(objs == min(objs),1),:);
    trace.objectives(iter)     = min(objs);
    trace.vals      (iter,:)   = objs;
    trace.locs      (iter,:,:) = locs;
+   
 end
 
 sol = best_loc;
 
+end
 
+function [nbrhd] = create_neighborhoods(nps, random, neighbor_fraction)
 
+    include_self = 1;
+    if random
+        nnbrs = floor(nps*neighbor_fraction);
+        nbrhd = NaN(nps, nnbrs);
+        for ip = 1:nps
+            nbrs = randperm(nps - 1, nnbrs);
+            nbrs(nbrs > ip) = nbrs(nbrs > ip) + 1;
+            nbrhd(ip,:) = nbrs;
+        end
+        if include_self
+            nbrhd = [nbrhd, (1:nps)'];
+        end
+    else
+        prime_fact = factor(nps);
+        vnt_dim1   = prod(prime_fact(1:end-1));
+        vnt_dim2   = prime_fact(end);
+        template   = zeros(1,nps);
+
+        for ip = 1:nps
+           b_ind = mod(ip+1,nps);
+           u_ind = mod(ip-1,nps);
+           r_ind = mod(ip+vnt_dim2,nps);
+           l_ind = mod(ip-vnt_dim2,nps);
+
+           nbrs = [b_ind, u_ind, r_ind, l_ind];
+           nbrs(nbrs == 0) = nps;
+           nbrhd(ip,:) = nbrs;
+        end
+    end
 end
--- opt_utils/alg_protos/test_wrappers/test_pso.m
+++ opt_utils/alg_protos/test_wrappers/test_pso.m
@@ -1,13 +1,14 @@
 function [ ] = test_pso(title_addendum)
 %TEST_PSO Summary of this function goes here
 %   Detailed explanation goes here
-n_repetitions = 500;
+n_repetitions = 200;
 bnds  = [0.5,3.5; -0.5,3];
 hist_edges = 0.0:0.05:1;
 max_dist_to_sol = sqrt(sum([[1,1] - bnds(:,1)'].^2));
 
 
-niter_set = [20,30,40,50,60,200];
+niter_set = [6,30,40,50,60,200];
+nps_set   = [4,8,16,32];
 figure();
 for niter_num = 1:length(niter_set)
 
@@ -21,7 +22,10 @@ for niter_num = 1:length(niter_set)
        gn    = @(x) rosenbrock(x');
 
        % Find the minimum.
-       [sol, ~] = particle_swarm(bnds,nps,niter,fn);
+       [sol, trace] = particle_swarm(bnds,niter,nps,fn);
+
+       %opts = optimoptions('particleswarm','MaxIter',niter,'SwarmSize',nps);
+       %sol = particleswarm(fn,2,bnds(:,1),bnds(:,2), opts);       
 
        solutions(i,:) = sol;
     end
@@ -44,73 +48,36 @@ end
 suptitle({'\bf{Dependence of solution quality on max iterations}', title_addendum})
 
 %%
-nps_set = [4,8,16,20,30,60];
-figure();
-for nps_num = 1:length(nps_set)
+if plot_loc
+    % Determine the x-domain and y-domain (the parameter space grid)
+    % used in plotting the fit surface.
+    xdom = -3:0.1:3;
+    ydom = -3:0.1:3;
 
-    nps = nps_set(nps_num);
-    solutions = NaN(n_repetitions,2);
-    
-    for i = 1:n_repetitions
-       niter = 40;
-       inc   = 0.1;
-       fn    = @(x) rosenbrock(x);
-       gn    = @(x) rosenbrock(x');
+    [meshVec1,meshVec2] = meshgrid(xdom,ydom);
 
-       % Find the minimum.
-       [sol, ~] = particle_swarm(bnds,nps,niter,fn);
+    % Get the actual objectives on the mesh-grid so we can plot the
+    % objective function along with the fit.
+    obj = rosenbrock([meshVec1(:),meshVec2(:)]);
+    obj = reshape(obj,length(xdom),length(ydom));
+ 
+    %-------------------------------------------------------------------------%
+    % Plot things
+    %-------------------------------------------------------------------------%
+    figure();
+    % Plot the Rosenbrock Function.
+    hold on
+    plot_2D_fn( bnds(1,:), bnds(2,:), inc, inc, 'incs', gn, 'none');
+    plot3(1, 1, rosenbrock([1,1]),'or')
+    title('\bf{Response Surface (Global)}')
 
-       solutions(i,:) = sol;
-    end
-    
-    subplot(2, length(nps_set)/2, nps_num)
-    data = histogram(sqrt(sum((solutions - [1,1]).^2,2)),hist_edges, 'Normalization', 'probability');
-    hold on; 
-    bar(data.BinEdges(1:end-1)+0.05,cumsum(data.Values),'FaceAlpha',0.3)
-
-    title(['nps = ', num2str(nps), ', niter = 40'])
-    xlabel({'Distance from Solution',['Max Possible ', num2str(max_dist_to_sol)]})
-    ylabel('Percent in Bin')
-    
-    legend({'hist','cdf'})
-    
-    ylim([0,1])
-    xlim([0,max(hist_edges)])
-    %disp(solutions)
+    th = 0:pi/50:2*pi;
+    xunit = 0.25 * cos(th) + 1;
+    yunit = 0.25 * sin(th) + 1;
+    h = plot3(xunit, yunit, repmat(40,length(th),1));
+    hold off
+    legend({'Objective fn', 'Solution', 'Circle with R = 0.25'})
 end
-suptitle({'\bf{Dependence of solution quality on num. particles}', title_addendum})
-
-
-%%
-% Determine the x-domain and y-domain (the parameter space grid)
-% used in plotting the fit surface.
-xdom = -3:0.1:3;
-ydom = -3:0.1:3;
-
-[meshVec1,meshVec2] = meshgrid(xdom,ydom);
-
-% Get the actual objectives on the mesh-grid so we can plot the
-% objective function along with the fit.
-obj = rosenbrock([meshVec1(:),meshVec2(:)]);
-obj = reshape(obj,length(xdom),length(ydom));
-
-%-------------------------------------------------------------------------%
-% Plot things
-%-------------------------------------------------------------------------%
-figure();
-% Plot the Rosenbrock Function.
-hold on
-plot_2D_fn( bnds(1,:), bnds(2,:), inc, inc, 'incs', gn, 'none');
-plot3(1, 1, rosenbrock([1,1]),'or')
-title('\bf{Response Surface (Global)}')
-
-th = 0:pi/50:2*pi;
-xunit = 0.25 * cos(th) + 1;
-yunit = 0.25 * sin(th) + 1;
-h = plot3(xunit, yunit, repmat(40,length(th),1));
-hold off
-legend({'Objective fn', 'Solution', 'Circle with R = 0.25'})
-
 
 end
 
